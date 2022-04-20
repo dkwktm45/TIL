2c7567a5881a3bbdd4caca9a7592f4613762d7fe
dkwktm45 <dkwktm45@naver.com>
2022-04-20 20:32:22 +0900

    web ui

--- security-project/server/paper-app/src/main/java/com/sp/fc/web/config/OnlinePaperSecurityConfig.java
+++ security-project/server/paper-app/src/main/java/com/sp/fc/web/config/OnlinePaperSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sp.fc.web.config;
 
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,8 +11,10 @@ public class OnlinePaperSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(request ->
+                .authorizeRequests(request->
                         request.anyRequest().permitAll()
                 );
     }
+
+
 }
