157cb822f15c912e76beb9c1602a703aae1edebf
jin young lee <dkwktm45@naver.com>
2024-02-16 21:50:28 +0900

    #3 #4 공변,무공변,반공변 이란 무엇인가?

--- src/main/kotlin/generic/Cage2.kt
+++ src/main/kotlin/generic/Cage2.kt
@@ -1,11 +1,17 @@
 package generic
 
 fun main() {
+  /*
+  * out : 생산자, 공변
+  * in : 소비자, 반공면
+  * */
+
+  // 변수에 공변과 반공변을 넣는 방법
   val cage: Cage2<out Fish> = Cage2<GoldFish>()
+  val cage2: Cage2<in GoldFish> = Cage2<Fish>()
 }
 
 class Cage2<T : Any> {
-  // Todo MutableList은 무엇이지?
   private val animals: MutableList<T> = mutableListOf()
 
   fun getFirst(): T {
@@ -14,5 +20,15 @@ class Cage2<T : Any> {
 
   fun put(animal: T) {
     this.animals.add(animal)
+  }
+
+  // 공변
+  fun moveFrom(otherCage: Cage2<out T>) {
+    this.animals.addAll(otherCage.animals)
+  }
+
+  // 반공변
+  fun moveTo(otherCage: Cage2<in T>) {
+    otherCage.animals.addAll(this.animals)
   }
 }
